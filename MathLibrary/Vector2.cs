61facc32a6ad98fb2019f5be8a7640881c32112e
drew-aie <159073200+drew-aie@users.noreply.github.com>
2024-10-14 15:12:17 -0500

    Add project files.

---
+++ MathLibrary/Vector2.cs
@@ -0,0 +1,80 @@
+﻿namespace MathLibrary
+{
+    public struct Vector2
+    {
+        float x, y;
+
+        public float Magnitude
+        {
+            get
+            {
+                return (float)Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            }
+        }
+
+        public Vector2 Normalized
+        {
+            get
+            {
+                return this / Magnitude;
+            }
+        }
+
+        public Vector2(float x = 0, float y = 0)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Vector2 Normalize()
+        {
+            this = Normalized;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return "(" + x + ", " + y + ")";
+        }
+
+        public static bool operator ==(Vector2 left, Vector2 right)
+        {
+            return (left.x == right.x) && (left.y == right.y);
+        }
+
+        public static bool operator !=(Vector2 left, Vector2 right)
+        {
+            return !(left == right);
+        }
+
+        public static Vector2 operator +(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.x + right.x, left.y + right.y);
+        }
+
+        public static Vector2 operator -(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.x - right.x, left.y - right.y);
+        }
+
+        public static Vector2 operator *(Vector2 left, float scalar)
+        {
+            return new Vector2(left.x * scalar, left.y * scalar);
+        }
+
+        public static Vector2 operator /(Vector2 left, float scalar)
+        {
+            return new Vector2(left.x / scalar, left.y / scalar);
+        }
+
+        public static implicit operator Vector2(System.Numerics.Vector2 vector)
+        {
+            return new Vector2(vector.X, vector.Y);
+        }
+
+        public static implicit operator System.Numerics.Vector2(Vector2 vector)
+        {
+            return new System.Numerics.Vector2(vector.x, vector.y);
+        }
+    }
+}
---
+++ Sandbox/Program.cs
@@ -0,0 +1,28 @@
+﻿using MathLibrary;
+using Raylib_cs;
+
+namespace Sandbox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 480, "Hello World");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.White);
+
+                Raylib.DrawText("Raylib is better than the console!", 12, 12, 20, Color.Black);
+                Raylib.DrawRectangle(100, 100, 100, 100, new Color(100, 200, 150, 100));
+                Raylib.DrawRectangle(150, 150, 100, 100, new Color(100, 200, 150, 100));
+                Raylib.DrawRectangleV(new Vector2(100, 100), new Vector2(150, 150), Color.Red);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
